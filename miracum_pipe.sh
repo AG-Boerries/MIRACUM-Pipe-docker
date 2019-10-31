143eb8cb2b0c025e55c29a92637b803a87e98e9e
scheiblr <raphael.scheible@uniklinik-freiburg.de>
2019-10-31 12:18:05 +0100

    docker repo as param

--- miracum_pipe.sh
+++ miracum_pipe.sh
@@ -2,18 +2,21 @@
 
 readonly DIR_MIRACUM="/opt/MIRACUM-Pipe"
 
+function join_by { local IFS="$1"; shift; echo "$*"; }
+
 function usage() {
-  docker run -it --name run-miracum-pipeline --rm miracumpipe:$1 "${DIR_MIRACUM}"/miracum_pipe.sh -h
+  docker run -it --name run-miracum-pipeline --rm $1:$2 "${DIR_MIRACUM}"/miracum_pipe.sh -h
   echo ""
   echo "additional optional flags:"
   echo "  -r                    set temporary folder into RAM"
+  echo "  -n                    docker repo name (default is agboerries/miracum_pipe)"
   echo "  -v version            specify version (default is \"latest\")"
   exit 1
 }
 
-function join_by { local IFS="$1"; shift; echo "$*"; }
+PARAM_DOCKER_REPO_NAME="agboerries/miracum_pipe"
 
-while getopts t:d:v:fsh option; do
+while getopts t:d:v:n:fsrh option; do
   case "${option}" in
   t) readonly PARAM_TASK=$OPTARG;;
   f) readonly PARAM_FORCED=true;;
@@ -21,6 +24,7 @@ while getopts t:d:v:fsh option; do
   v) PIPELINE_VERSION=$OPTARG;;
   r) readonly PARAM_RAM=$OPTARG;;
   s) readonly PARAM_SEQ=true;;
+  n) PARAM_DOCKER_REPO_NAME=$OPTARG;;
   h) readonly SHOW_USAGE=true;;
    \?)
     echo "Unknown option: -$OPTARG" >&2
@@ -38,7 +42,7 @@ while getopts t:d:v:fsh option; do
 done
 
 [[ -z "${PIPELINE_VERSION}" ]] && PIPELINE_VERSION='latest'
-[[ "${SHOW_USAGE}" ]] && usage "${PIPELINE_VERSION}"
+[[ "${SHOW_USAGE}" ]] && usage "${PARAM_DOCKER_REPO_NAME}" "${PIPELINE_VERSION}"
 
 # conf as volume
 if [[ -d $(pwd)/conf ]]; then
@@ -67,7 +71,7 @@ if [[ "${PARAM_RAM}" ]]; then
   readonly TMP_RAM="--tmpfs /tmp:exec"
 fi
 
-echo "running \"${DIR_MIRACUM}/miracum_pipe.sh ${opt_args}\" inside docker image miracumpipe:${PIPELINE_VERSION}"
+echo "running \"${DIR_MIRACUM}/miracum_pipe.sh ${opt_args}\" of docker miracumpipe:${PIPELINE_VERSION}"
 echo "---"
 docker run -it --name run-miracum-pipeline --rm ${TMP_RAM} ${VOLUME_CONF} \
   -u $(id -u $USER) \
@@ -76,4 +80,4 @@ docker run -it --name run-miracum-pipeline --rm ${TMP_RAM} ${VOLUME_CONF} \
   -v "$(pwd)/assets/references:${DIR_MIRACUM}/assets/references" \
   -v "$(pwd)/tools/annovar:${DIR_MIRACUM}/tools/annovar" \
   -v "$(pwd)/tools/gatk:${DIR_MIRACUM}/tools/gatk" \
-  -v "$(pwd)/databases:${DIR_MIRACUM}/databases" miracumpipe:"${PIPELINE_VERSION}" "${DIR_MIRACUM}/miracum_pipe.sh" ${opt_args}
+  -v "$(pwd)/databases:${DIR_MIRACUM}/databases" ${PARAM_DOCKER_REPO_NAME}:"${PIPELINE_VERSION}" "${DIR_MIRACUM}/miracum_pipe.sh" ${opt_args}
